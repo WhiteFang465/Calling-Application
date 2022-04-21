3ace2e32e870dea44a6a33cce9e87b8c068c5baa
Atul Jairam <shadesofaura@gmail.com>
2022-04-21 00:03:02 -0400

    appbarmenu.xml created the UI

--- app/src/main/java/com/atulj/callingapplication/MainActivity.java
+++ app/src/main/java/com/atulj/callingapplication/MainActivity.java
@@ -4,18 +4,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
-
-import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
     private String name;
@@ -44,6 +46,21 @@ public class MainActivity extends AppCompatActivity {
             }
     );
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater()
+                .inflate(R.menu.appbarmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.more) {
+            Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
+
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         register = findViewById(R.id.MainActivityRegisterButton);
+        textView = findViewById(R.id.MainActivityTextView);
 
         register.setOnClickListener(this::changeActivityToRegister);
 
@@ -60,24 +78,29 @@ public class MainActivity extends AppCompatActivity {
 
         Log.e("Name", "Name " + name);
         Log.e("phone", "phone :" + phone);
+
+
         if (getData.hasExtra(RegisterActivity.EXTRA_NAME) || getData.hasExtra(RegisterActivity.EXTRA_NUMBER)) {
 
 
-             name = getData.getExtras().getString(RegisterActivity.EXTRA_NAME);
-             phone = getData.getExtras().getString(RegisterActivity.EXTRA_NUMBER);
+            name = getData.getExtras().getString(RegisterActivity.EXTRA_NAME);
+            phone = getData.getExtras().getString(RegisterActivity.EXTRA_NUMBER);
             register.setText("Logout");
             Log.e("Registration done", " done registration");
             Log.e("Name", "Name " + name);
             Log.e("phone", "phone :" + phone);
+            textView.setText("Hello " + name);
 
             call.setOnClickListener(v -> {
                 Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
-                Snackbar.make(register,"Call", BaseTransientBottomBar.LENGTH_INDEFINITE)
-                        .setAction(R.string.call_label,v1 -> {
-                            phoneIntent.setData(Uri.parse("tel:"+phone));
+                Snackbar.make(register, "Call", BaseTransientBottomBar.LENGTH_INDEFINITE)
+                        .setAction(R.string.call_label, v1 -> {
+                            phoneIntent.setData(Uri.parse("tel:" + phone));
                             startActivity(phoneIntent);
                         })
                         .show();
+
+
             });
 
         } else {
@@ -85,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
             Log.e("phone", "phone :" + phone);
             Log.e("Not registered", "not registered");
             call.setOnClickListener(v -> {
-                Snackbar.make(register,"Please Register to use this feature", BaseTransientBottomBar.LENGTH_INDEFINITE)
+                Snackbar.make(register, "Please Register to use this feature", BaseTransientBottomBar.LENGTH_INDEFINITE)
                         .show();
             });
         }
@@ -96,4 +119,5 @@ public class MainActivity extends AppCompatActivity {
         getRegisteredDetails.launch(goToRegisterScreen);
 
     }
+
 }
